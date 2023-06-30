59238bbc17d7771310e0b9f3242b4973160535cd
Bruce Hsu <1033684650@qq.com>
2023-06-30 09:51:02 +0800

    modified the tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,34 @@
-/** @type {import('tailwindcss').Config} */
+/*
+ * @Date: 2023-06-30 09:37:33
+ * @Author: Bruce Hsu
+ * @Description: 
+ */
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class", '[data-theme="dark"]'],
+  content: ["src/components/**/*.{ts, tsx}", "src/pages/**/*.{ts, tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" }
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 }
+        }
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out"
+      }
+    }
   },
-  plugins: [],
-};
+  // @ts-ignore
+  plugins: [require("tailwindcss-animate")]
+}
