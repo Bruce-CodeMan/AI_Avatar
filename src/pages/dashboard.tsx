4e17248e5b256c4a6218dd5693dcff5d19d506bd
Bruce <1033684650@qq.com>
2023-07-01 23:03:11 +0800

    add loading icons in the dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { ImSpinner9 } from "react-icons/im";
+
+// Custom Imports
+import Layout from "@/components/layout";
 
 const Dashboard = () => {
+
+    const [ loading, setLoading ] = useState(false);
+
+    if(loading) {
+        return (
+            <Layout>
+                <div className="h-[80vh] w-full flex items-center justify-center">
+                    <ImSpinner9 className="h-10 w-10 animate-spin"/>
+                </div>
+            </Layout>
+        )
+    }
+
     return (
-        <div>Dashboard</div>
+        <Layout>
+            {/* this is protected page, only for the users who have done the payment */}
+        </Layout>
     )
 }
 
